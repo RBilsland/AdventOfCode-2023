8509bb5f0eedc928918c66205cbf01df42a4209b
Robert Bilsland <RCBilsland@GMail.com>
2023-12-04 10:49:42 +0000

    Day04a

--- Day04a/Program.cs
+++ Day04a/Program.cs
@@ -6,6 +6,18 @@ class Program
     {
         Console.WriteLine("Day 04a");
 
+        string[] input = File.ReadAllLines("input.txt");
+
+        var points = input
+            .Select(c => c.Split(':', '|'))
+            .Select(c => new { No = int.Parse(c[0].Replace("Card ", "")), Winning = Array.ConvertAll(c[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s)), Chosen = Array.ConvertAll(c[2].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s)) })
+            .Select(c => c.Chosen.Intersect(c.Winning).Count())
+            .Where(c => c > 0)
+            .Select(c => c == 1 ? 1 : Math.Pow(2, c - 1))
+            .Sum();
+
+        Console.WriteLine(points);
+
         Console.WriteLine("Complete");
     }
 }
