4d0b58d53c9c6f679cb477dd501a21076c873e7d
Robert Bilsland <RCBilsland@GMail.com>
2023-12-06 18:27:24 +0000

    Day06a

--- Day06a/Program.cs
+++ Day06a/Program.cs
@@ -10,6 +10,33 @@ class Program
 
         string[] input = File.ReadAllLines("input.txt");
 
+        int[] time = Array.ConvertAll(input[0].Replace("Time:", "").Split(' ', StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s));
+        int[] distance = Array.ConvertAll(input[1].Replace("Distance:", "").Split(' ', StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s));
+
+        int runningTotal = 1;
+
+        for(int i = 0; i < time.Length; i++)
+        {
+            int position = (int)Math.Floor(time[i] / 2.0);
+
+            double maxDistance = position * (time[i] - position);
+
+            if (maxDistance > distance[i])
+            {
+                var calculatedDistance = maxDistance;
+
+                while(calculatedDistance > distance[i] && position >= 0)
+                {
+                    position--;
+                    calculatedDistance = position * (time[i] - position);
+                }
+
+                runningTotal *= (time[i] + 1) - ((position + 1) * 2);
+            }
+        }
+
+        Console.WriteLine(runningTotal);
+
         Console.WriteLine("Complete");
     }
 }
