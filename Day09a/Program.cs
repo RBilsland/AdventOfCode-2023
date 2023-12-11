9720322878b5339416504721c7981fe1d69d5c26
Robert Bilsland <RCBilsland@GMail.com>
2023-12-11 11:40:40 +0000

    Day09a

--- Day09a/Program.cs
+++ Day09a/Program.cs
@@ -8,6 +8,42 @@ class Program
 
         string[] input = File.ReadAllLines("input.txt");
 
+        List<long> values;
+        long runningTotal = 0;
+
+        foreach(string line in input)
+        {
+            values = line
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => long.Parse(s))
+                .ToList();
+
+            runningTotal += CalculateNextDigit(values);
+        }
+
+        Console.WriteLine(runningTotal);
+
         Console.WriteLine("Complete");
+    }
+
+    public static long CalculateNextDigit(List<long> calculationValues)
+    {
+        List<long> valueDifferences = [];
+
+        for(int i = 1; i < calculationValues.Count; i++)
+        {
+            valueDifferences.Add(calculationValues[i] - calculationValues[i - 1]);
+        }
+
+        long returnValue = calculationValues.Last();
+
+        if (valueDifferences
+            .Where(v => v == 0)
+            .Count() != valueDifferences.Count)
+        {
+            returnValue += CalculateNextDigit(valueDifferences);
+        }
+
+        return returnValue;
     }
 }
